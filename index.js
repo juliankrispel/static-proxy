2bd53993debb44bb1e04ae39bbbab4ef108a1365
Julian Krispel-Samsel <juliankrispel@yahoo.de>
2015-06-21 18:48:17 -0700

    forward body and cookies

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 var express = require('express');
 var request = require('request');
+var cookieParser = require('cookie-parser');
+var tough = require('tough-cookie');
+var Cookie = tough.Cookie;
 var _ = require('lodash');
 var path = require('path');
 var app = express();
 var colors = require('colors');
 var protocol = 'https://';
 var baseUrl = 'app.rnfrstqa.com'
+var bodyParser = require('body-parser')
+
+app.use(cookieParser());
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 var url = function(url){
   return protocol + path.join(baseUrl, url);
 };
 
+var j = request.jar();
+
 var makeRequest = function(req, res){
   console.log(colors.yellow('Requesting url >> ', url(req.url)));
-  console.log(req.cookies);
+  // assign cookies to cookiejar
+  _.forEach(req.cookies, function(value, key){
+    j.setCookie(key + '=' + value, baseUrl);
+  });
+
   request({
     method: req.method,
-    uri: url(req.url)
+    jar: j,
+    uri: url(req.url),
+    form: req.body
   }, function (error, response, body) {
     if(error){
       console.log(colors.red(error));
